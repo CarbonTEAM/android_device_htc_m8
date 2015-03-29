3a8eb42b9c91fcc9587db08f99ab2bd01eb5951b
STELIX <ssspinni@gmail.com>
2015-03-29 19:17:39 +0200

    HTC M8 - Carbonize CM
    Change-Id: I752635704facfc8831230f0b61c4cf7834f6fc72
    Signed-off-by: STELIX <ssspinni@gmail.com>

---
+++ carbon.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/htc/m8/full_m8.mk)
+
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+PRODUCT_NAME := carbon_m8
+PRODUCT_DEVICE := m8
