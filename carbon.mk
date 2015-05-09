487fd2adb2a35c3a60731eeedc9d5317d5f05899
STELIX <ssspinni@gmail.com>
2015-05-09 18:36:44 +0200

    M8 - STELIX --> CarbonROM
    Change-Id: If78b6831f8d3c0dc30098c10af94e894cd9dae24
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- carbon.mk
+++ carbon.mk
@@ -16,10 +16,10 @@
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
-#$(call inherit-product, vendor/stelix/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
 
 # Inherit some common Carbon stuff.
-$(call inherit-product, vendor/stelix/config/common_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_NAME := carbon_m8
 PRODUCT_DEVICE := m8
